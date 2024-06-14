18381db1ac5cca13902a7c7cece4e4b01ca430ef
Tinh Nguyen <nguyenantinhbk77@gmail.com>
2024-06-15 00:28:05 +0700

    init project with assests

---
+++ Simplist/Simplist/SimplistApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplistApp.swift
+//  Simplist
+//
+//  Created by Tinh Nguyen on 15/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SimplistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
